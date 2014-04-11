9083084686beace03f47493b97c2cee6abf16624
dhuy <vandhuy@6ps.com>
2014-04-11 02:45:18 -0300

    Test in video and subtitle endpoint

--- server.js
+++ server.js
@@ -185,7 +185,7 @@ app.post('/api', function(req, res){
 
 									/* Cria a linha de comando */
 									var command_line =  'vlibras_user/vlibras-core/./gtaaas ' + parameters.getServiceType(req.query.servico) + ' uploads/' + ID_FROM_BD + '/' +
-													req.files.video.name + ' uploads/' + ID_FROM_BD + '/' + req.files.legenda.name + ' ' + parameters.getLanguage(req.params.linguagem) +
+													req.files.video.name + ' uploads/' + ID_FROM_BD + '/' + req.files.legenda.name + ' ' + parameters.getLanguage(req.query.linguagem) +
 													' ' + parameters.getPosition(req.query.posicao) + ' ' + parameters.getSize(req.query.tamanho) + ' ' +
 													parameters.getTransparency(req.query.transparencia) + ' ' + ID_FROM_BD;
 
