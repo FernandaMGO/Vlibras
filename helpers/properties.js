6ea5093684ab4c99ece2ff820a82f4b5ae41d4c0
Erickson Silva <erickson.silva@lavid.ufpb.br>
2015-01-07 11:34:22 -0300

    Corrige URL de retorno

--- helpers/properties.js
+++ helpers/properties.js
@@ -2,8 +2,9 @@ var host = '0.0.0.0';
 var port = 5000;
 var SERVER_IP = '150.165.204.30';
 var uploads_folder = './uploads/';
+var ip = require('ip');
 
 module.exports.host = host;
 module.exports.port = port;
 module.exports.uploads_folder = uploads_folder;
-module.exports.SERVER_IP = SERVER_IP;
+module.exports.SERVER_IP = ip.address();
